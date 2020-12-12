648eccd61ae713bfb7121804d134d7f42f761be6
April <buzdav2@gmail.com>
2020-12-12 02:58:45 -0600

    solve 8.2

--- src/day8.zig
+++ src/day8.zig
@@ -9,7 +9,8 @@ const ArrayList = std.ArrayList;
 const Allocator = std.mem.Allocator;
 
 const Solution = struct {
-    final_reg: i32,
+    final_reg_loop: i32,
+    final_reg_uncorrupted: i32,
 };
 
 pub fn solve(alloc: *Allocator) !Solution {
@@ -24,8 +25,12 @@ pub fn solve(alloc: *Allocator) !Solution {
 
     var prog_state = State{ .insts = inst_arr.items };
 
+    const loop_result = try prog_state.run(alloc);
+    const final_reg_uncorrupted = try prog_state.run_uncorrupted(alloc);
+
     return Solution{
-        .final_reg = try prog_state.run_until_loop(alloc),
+        .final_reg_loop = loop_result.final_acc,
+        .final_reg_uncorrupted = final_reg_uncorrupted,
     };
 }
 
@@ -50,17 +55,46 @@ const State = struct {
     acc: i32 = 0,
     insts: []Inst,
 
-    fn run_until_loop(self: *State, alloc: *Allocator) !i32 {
-        var hit_insts = try alloc.alloc(bool, self.insts.len);
+    fn run_uncorrupted(self: *State, alloc: *Allocator) !i32 {
+        const hit_insts = try alloc.alloc(bool, self.insts.len);
         defer alloc.free(hit_insts);
 
+        for (self.insts) |*inst| {
+            inst.flip();
+            defer inst.flip();
+            self.reset();
+            const run_result = self.run_with_hit_list(hit_insts);
+            if (run_result.finished) {
+                return run_result.final_acc;
+            }
+        }
+        return error.Unsalvageable;
+    }
+
+    fn run(self: *State, alloc: *Allocator) !RunResult {
+        const hit_insts = try alloc.alloc(bool, self.insts.len);
+        defer alloc.free(hit_insts);
+        return self.run_with_hit_list(hit_insts);
+    }
+
+    fn run_with_hit_list(self: *State, hit_insts: []bool) RunResult {
         mem.set(bool, hit_insts, false);
-        while (!hit_insts[self.ip]) {
+        const finished = while (self.ip < self.insts.len) {
+            if (hit_insts[self.ip])
+                break false;
             hit_insts[self.ip] = true;
             self.step();
-        } 
-        return self.acc;
+        } else true;
+        return RunResult{
+            .final_acc = self.acc,
+            .finished = finished,
+        };
     }
+
+    const RunResult = struct {
+        final_acc: i32,
+        finished: bool,
+    };
 
     fn step(self: *State) void {
         const cur = self.insts[self.ip];
@@ -78,7 +112,20 @@ const State = struct {
         }
     }
 
+    fn reset(self: *State) void {
+        self.acc = 0;
+        self.ip = 0;
+    }
+
     const Inst = struct {
+        fn flip(self: *Inst) void {
+            self.kind = switch (self.kind) {
+                .Nop => .Jmp,
+                .Jmp => .Nop,
+                .Acc => .Acc,
+            };
+        }
+
         const Kind = enum { Nop, Jmp, Acc };
         kind: Kind,
         val: isize,
