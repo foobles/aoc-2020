5b8f383a20d063c5c63ad639e8ffc90dd86920a6
foobles <buzdav2@gmail.com>
2020-12-14 12:35:56 -0600

    solve 10.1

---
+++ src/day10.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+const file_util = @import("file_util.zig");
+
+const fmt = std.fmt;
+const sort = std.sort;
+
+const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
+
+pub const Solution = struct {
+    diff_product: usize,
+};
+
+pub fn solve(alloc: *Allocator) !Solution {
+    var jolt_list = ArrayList(usize).init(alloc);
+    defer jolt_list.deinit();
+
+    var line_iter = try file_util.dayFileLines(alloc, 10, "adapters.txt");
+    defer line_iter.deinit();
+
+    try jolt_list.append(0);
+    while (try line_iter.next()) |line| {
+        try jolt_list.append(try fmt.parseUnsigned(usize, line, 10));
+    }
+    sort.sort(usize, jolt_list.items, {}, comptime sort.asc(usize));
+    try jolt_list.append(jolt_list.items[jolt_list.items.len - 1] + 3);
+
+    for (jolt_list.items) |n| std.log.debug("{}", .{n});
+
+    return Solution{
+        .diff_product = countJoltDiffProductAssumeSorted(jolt_list.items),
+    };
+}
+
+fn countJoltDiffProductAssumeSorted(jolts: []const usize) usize {
+    if (jolts.len == 0)
+        return 1;
+
+    var diff_1_count: usize = 0;
+    var diff_3_count: usize = 0;
+    for (jolts[1..]) |cur, i| {
+        const prev = jolts[i];
+        const diff = cur - prev;
+        if (diff == 1) {
+            diff_1_count += 1;
+        } else if (diff == 3) {
+            diff_3_count += 1;
+        }
+    }
+    return diff_1_count * diff_3_count;
+}
--- src/main.zig
+++ src/main.zig
@@ -16,6 +16,7 @@ const days = [_]type{
     @import("day7.zig"),
     @import("day8.zig"),
     @import("day9.zig"),
+    @import("day10.zig"),
 };
 
 pub fn dumpSolutions(alloc: *std.mem.Allocator) !void {
