35741ad1b4b88741635699497ed151409028c037
foobles <buzdav2@gmail.com>
2020-12-14 13:15:40 -0600

    solve 10.2

--- src/day10.zig
+++ src/day10.zig
@@ -3,12 +3,14 @@ const file_util = @import("file_util.zig");
 
 const fmt = std.fmt;
 const sort = std.sort;
+const mem = std.mem;
 
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
 
 pub const Solution = struct {
     diff_product: usize,
+    arrangements: u64,
 };
 
 pub fn solve(alloc: *Allocator) !Solution {
@@ -25,10 +27,9 @@ pub fn solve(alloc: *Allocator) !Solution {
     sort.sort(usize, jolt_list.items, {}, comptime sort.asc(usize));
     try jolt_list.append(jolt_list.items[jolt_list.items.len - 1] + 3);
 
-    for (jolt_list.items) |n| std.log.debug("{}", .{n});
-
     return Solution{
         .diff_product = countJoltDiffProductAssumeSorted(jolt_list.items),
+        .arrangements = try countJoltArrangementsAssumeSorted(alloc, jolt_list.items),
     };
 }
 
@@ -48,4 +49,26 @@ fn countJoltDiffProductAssumeSorted(jolts: []const usize) usize {
         }
     }
     return diff_1_count * diff_3_count;
+}
+
+fn countJoltArrangementsAssumeSorted(alloc: *Allocator, jolts: []const usize) !u64 {
+    if (jolts.len == 0)
+        return @as(u64, 1);
+
+    const map = try alloc.alloc(u64, jolts[jolts.len - 1] + 1);
+    defer alloc.free(map);
+    mem.set(u64, map, 0);
+
+    map[jolts[0]] = 1;
+    for (jolts[1..]) |jolt| {
+        var cur_count: u64 = 0;
+        var i: usize = 1;
+        while (i <= 3) : (i += 1) {
+            if (jolt < i)
+                break;
+            cur_count += map[jolt - i];
+        }
+        map[jolt] = cur_count;
+    }
+    return map[map.len - 1];
 }
